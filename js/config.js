dbc3abb41f10b7cc80de877ccd54dcc7202eb31f
김유진 <50358936+kluedeugene@users.noreply.github.com>
2020-08-31 15:35:32 +0900

    Add files via upload
    코그니토 사용 연습 예제

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_alLPoMsaE', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-2', // e.g. us-east-2
+		clientId: 'jd2e1po4ddtq5u9l3020kr1u0' //is this used anywhere?
+    },
+};
+
